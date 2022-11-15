39838fd4e3fa36ca25ff667aea36f7f0471f3b02
Gerard Solanes Hernandez <gerardsolher@gmail.com>
2022-11-15 21:28:25 +0100

    Finish lab. Ready for bouns

--- src/chronometer.js
+++ src/chronometer.js
@@ -7,7 +7,9 @@ class Chronometer {
   start(printTimeCallback) {
     this.intervalId = setInterval(() => {
       this.currentTime += 1;
-      printTimeCallback();
+      if (printTimeCallback) {
+        printTimeCallback();
+      }
     }, 1000);
   }
 
@@ -20,15 +22,19 @@ class Chronometer {
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value < 10) {
+      return '0'+value;
+    } else {
+      return value.toString();
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
